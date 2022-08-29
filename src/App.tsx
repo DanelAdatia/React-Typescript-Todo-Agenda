8614718df042e9720a69ff0370bd0499f216d70f
Danel Adatia <daneladatia@gmail.com>
2022-08-29 18:37:00 +0530

    todo list agenda

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./App.css";
+import { Todo } from "./Models";
+import Input from "./view/Input";
+import TodoList from "./view/TodoList";
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+  console.log(todos);
+
+  return (
+    <div className="App">
+      <h1 className="heading">Agenda</h1>
+      <Input todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
+  );
+};
+
+export default App;
